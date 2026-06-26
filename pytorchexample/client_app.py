597d26fb63f1a5dbc2bd7c174e4b8417881f72cd
JoeSharp96 <joesharp1996@gmail.com>
2026-06-26 15:07:39 +0100

    inital build

---
+++ pytorchexample/client_app.py
@@ -0,0 +1,125 @@
+"""pytorchexample: A Flower / PyTorch app."""
+
+import torch
+from flwr.app import ArrayRecord, Context, Message, MetricRecord, RecordDict
+from flwr.clientapp import ClientApp
+
+from pytorchexample.task import Net, load_data
+from pytorchexample.task import test as test_fn
+from pytorchexample.task import train as train_fn
+from opacus import PrivacyEngine
+
+# Flower ClientApp
+app = ClientApp()
+
+
+@app.train()
+def train(msg: Message, context: Context):
+    """Train the model on local data."""
+    
+    # Load the model and initialize it with the received weights
+    model = Net()
+    model.load_state_dict(msg.content["arrays"].to_torch_state_dict())
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    # Load the data
+    partition_id = context.node_config["partition-id"]
+    num_partitions = context.node_config["num-partitions"]
+    batch_size = context.run_config["batch-size"]
+    trainloader, _ = load_data(partition_id, num_partitions, batch_size)
+
+    max_physical_batch_size = context.run_config["max_physical_batch_size"]
+    lr = msg.content["config"]["lr"]
+    epochs = context.run_config["local-epochs"]
+    train_loss = train_fn(
+        model,
+        trainloader,
+        epochs,
+        lr,
+        device,
+        max_physical_batch_size,
+        context
+    )
+
+    # Construct and return reply Message
+    #arrays = {key[8:]: value for key, value in model.state_dict().items()}
+    model_record = ArrayRecord(model.state_dict())
+    metrics = {
+        "train_loss": train_loss,
+        "num-examples": len(trainloader.dataset),
+    }
+    metric_record = MetricRecord(metrics)
+    content = RecordDict({"arrays": model_record, "metrics": metric_record})
+    return Message(content=content, reply_to=msg)
+"""
+    # Privacy hyperparams
+    epsilon = context.run_config["epsilon"]
+    delta = context.run_config["delta"]
+    max_grad_norm = context.run_config["max_grad_norm"]
+    max_physical_batch_size = context.run_config["max_physical_batch_size"]
+    lr = msg.content["config"]["lr"]
+    epochs = context.run_config["local-epochs"]
+    # Privacy engine
+    privacy_engine = PrivacyEngine()
+    optimizer = torch.optim.RMSprop(model.parameters(), lr=lr)
+
+    model, optimizer, trainloader = privacy_engine.make_private_with_epsilon(
+        module=model,
+        optimizer=optimizer,
+        data_loader=trainloader,
+        epochs = epochs,
+        target_epsilon=epsilon,
+        target_delta=delta,
+        max_grad_norm=max_grad_norm
+    )
+
+    # Call the training function
+    train_loss = train_fn(
+        model,
+        trainloader,
+        epochs,
+        lr,
+        device,
+        optimizer,
+        max_physical_batch_size,
+        epsilon,
+        delta,
+        privacy_engine
+    )"""
+
+
+
+
+@app.evaluate()
+def evaluate(msg: Message, context: Context):
+    """Evaluate the model on local data."""
+
+    # Load the model and initialize it with the received weights
+    model = Net()
+    model.load_state_dict(msg.content["arrays"].to_torch_state_dict())
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    # Load the data
+    partition_id = context.node_config["partition-id"]
+    num_partitions = context.node_config["num-partitions"]
+    batch_size = context.run_config["batch-size"]
+    _, valloader = load_data(partition_id, num_partitions, batch_size)
+
+    # Call the evaluation function
+    eval_loss, eval_acc = test_fn(
+        model,
+        valloader,
+        device,
+    )
+
+    # Construct and return reply Message
+    metrics = {
+        "eval_loss": eval_loss,
+        "eval_acc": eval_acc,
+        "num-examples": len(valloader.dataset),
+    }
+    metric_record = MetricRecord(metrics)
+    content = RecordDict({"metrics": metric_record})
+    return Message(content=content, reply_to=msg)
---
+++ pytorchexample/server_app.py
@@ -0,0 +1,85 @@
+"""pytorchexample: A Flower / PyTorch app."""
+
+import torch
+from flwr.app import ArrayRecord, ConfigRecord, Context, MetricRecord
+from flwr.serverapp import Grid, ServerApp
+from flwr.serverapp.strategy import FedAvg
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from pytorchexample.task import Net, load_centralized_dataset, test
+
+# Create ServerApp
+app = ServerApp()
+
+
+@app.main()
+def main(grid: Grid, context: Context) -> None:
+    """Main entry point for the ServerApp."""
+
+    # Read run config
+    fraction_evaluate: float = context.run_config["fraction-evaluate"]
+    num_rounds: int = context.run_config["num-server-rounds"]
+    lr: float = context.run_config["learning-rate"]
+
+    # Load global model
+    global_model = Net()
+    arrays = ArrayRecord(global_model.state_dict())
+
+    # Initialize FedAvg strategy
+    strategy = FedAvg(fraction_evaluate=fraction_evaluate)
+
+    # Start strategy, run FedAvg for `num_rounds`
+    result = strategy.start(
+        grid=grid,
+        initial_arrays=arrays,
+        train_config=ConfigRecord({"lr": lr}),
+        num_rounds=num_rounds,
+        evaluate_fn=global_evaluate,
+    )
+
+    if context.run_config["save-model"]:
+        # Save final model to disk
+        print("\nSaving final model to disk...")
+        state_dict = result.arrays.to_torch_state_dict()
+        torch.save(state_dict, "final_model.pt")
+
+    agg_acc = []
+    for round in result.evaluate_metrics_clientapp.values():
+        agg_acc.append(round['eval_acc'])
+
+    epochs = context.run_config['local-epochs']
+    if context.run_config['dp-enabled']:
+        epsilon = context.run_config['epsilon']
+        text = f"Sever rounds = {num_rounds}\nLocal epochs = {epochs}\nε = {epsilon}"
+    else:
+        text = f"Sever rounds = {num_rounds}\nLocal epochs = {epochs}\nNon-DP"
+
+    plt.figure(figsize=(5, 5))
+    plt.plot(pd.DataFrame(np.array(agg_acc)), marker='o', color='b', label='Round Accuracy')
+    plt.ylim(0, 1)
+    plt.xlabel('Round')
+    plt.ylabel('Accuracy')
+    plt.title('Training Accuracy vs Round')
+    plt.text(0,0.85,text)
+    plt.grid(True)
+    plt.legend()
+    plt.savefig("output.jpg")
+
+def global_evaluate(server_round: int, arrays: ArrayRecord) -> MetricRecord:
+    """Evaluate model on central data."""
+
+    # Load the model and initialize it with the received weights
+    model = Net()
+    model.load_state_dict(arrays.to_torch_state_dict())
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    # Load entire test set
+    test_dataloader = load_centralized_dataset()
+
+    # Evaluate the global model on the test set
+    test_loss, test_acc = test(model, test_dataloader, device)
+
+    # Return the evaluation metrics
+    return MetricRecord({"accuracy": test_acc, "loss": test_loss})
